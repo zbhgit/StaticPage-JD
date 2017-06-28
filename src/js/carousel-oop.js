e831b456101e3dfab9117c994e776aeefacf46ac
zbhgit <zbh_wj@163.com>
2017-06-28 21:10:58 +0800

    '完成seckill部分'

---
+++ src/js/carousel-oop.js
@@ -0,0 +1,110 @@
+var $carouselWrapper = $('.seckill-carousel')
+var $carouselItem = $carouselWrapper.find('li')
+var $prev = $('.seckill-prev')
+var $next = $('.seckill-next')
+
+// 初始num，点击按钮改变
+
+var initNum = 1
+var showNum = 5
+
+// 动画进行时
+
+var animate = false
+// 克隆元素
+
+// 每个item的宽度
+var itemWidth = $carouselItem.first().outerWidth()
+var itemLength = $carouselWrapper.find('li').length
+
+
+// clone前面n个item和后面n个元素 n为显示个数 
+var appendArr = []
+var prependArr = []
+$carouselItem.each(function (index) {
+  $(this).attr('data-index', index)
+  var newElemnt = $(this).clone()
+  if (index < 5) {
+    appendArr.push(newElemnt)
+  }
+  if (index > (itemLength - 1 - 5)) {
+    prependArr.push(newElemnt)
+  }
+})
+// 插入前面n个元素
+for (var j = prependArr.length - 1; j >= 0; j--) {
+  prependArr[j].prependTo('.seckill-carousel')
+}
+// 添加后面几个元素
+for (var i = 0; i < appendArr.length; i++) {
+  appendArr[i].appendTo('.seckill-carousel')
+}
+
+var newLength = $carouselWrapper.find('li').length
+
+// 设置carousel总长度宽度
+$carouselWrapper.css({
+  'width': newLength * itemWidth + 'px',
+  'transform': 'translateX(' + -itemWidth * showNum + 'px)'
+
+})
+// $carouselWrapper.css({
+//   'transition': 'transform 0.3s linear'
+
+// })
+
+
+// 按钮点击事件
+
+$next.on('click', function () {
+  if (animate === true) {
+    return
+  }
+  animate = true
+  console.log(initNum)
+  initNum += 1
+  $carouselWrapper.css({
+    'transition': 'transform 0.6s',
+    'transform': 'translateX(' + -itemWidth * showNum * initNum + 'px)'
+  })
+  if (initNum >= newLength / showNum - 1) {
+    initNum = 1
+
+    setTimeout(function () {
+      $carouselWrapper.css({
+        'transition': 'none',
+        'transform': 'translateX(' + -itemWidth * showNum + 'px)'
+      })
+    }, 600)
+  }
+  setTimeout(function () {
+    animate = false
+  }, 600)
+})
+$prev.on('click', function () {
+  if (animate === true) {
+    return
+  }
+  animate = true
+  initNum--
+  $carouselWrapper.css({
+    'transition': 'transform 0.6s',
+    'transform': 'translateX(' + -itemWidth * showNum * initNum + 'px)'
+  })
+  if (initNum === 0) {
+    initNum = 4
+
+    setTimeout(function () {
+      $carouselWrapper.css({
+        'transition': 'none',
+        'transform': 'translateX(' + -itemWidth * showNum * initNum + 'px)'
+      })
+    }, 600)
+  }
+  setTimeout(function () {
+    animate = false
+  }, 600)
+})
+
+
+//  倒计时程序
---
+++ src/js/fnTimeCountDown.js
@@ -0,0 +1,77 @@
+var fnTimeCountDown = function (d, o) {
+  console.log(2)
+  var f = {
+    zero: function (n) {
+      var n = parseInt(n, 10)
+      if (n > 0) {
+        if (n <= 9) {
+          n = '0' + n;
+        }
+        return String(n)
+      } else {
+        return "00"
+      }
+    },
+    dv: function () {
+      d = d || Date.UTC(2050, 0, 1) // 设定目标时间
+      var future = new Date(d)
+      var now = new Date()
+      var dur = Math.round((future.getTime() - now.getTime()) / 1000) + future.getTimezoneOffset() * 60
+      var pms = {
+        sec: "00",
+        mini: '00',
+        hour: '00',
+        day: "00",
+        month: "00",
+        year: '0'
+      }
+      if (dur > 0) {
+        pms.sec = f.zero(dur % 60)
+        pms.mini = Math.floor((dur / 60)) > 0 ? f.zero(Math.floor((dur / 60)) % 60) : "00"
+        pms.hour = Math.floor((dur / 3600)) > 0 ? f.zero(Math.floor((dur / 3600)) % 24) : "00"
+        pms.day = Math.floor((dur / 86400)) > 0 ? f.zero(Math.floor((dur / 86400)) % 30) : "00"
+        //月份，以实际平均每月秒数计算
+        pms.month = Math.floor((dur / 2629744)) > 0 ? f.zero(Math.floor((dur / 2629744)) % 12) : "00"
+        //年份，按按回归年365天5时48分46秒算
+        pms.year = Math.floor((dur / 31556926)) > 0 ? Math.floor((dur / 31556926)) : "0"
+      }
+      return pms
+    },
+    ui: function(){
+      console.log(1)
+      if(o.sec) {
+        o.sec.innerHTML = f.dv().sec
+      }
+      if(o.mini) {
+        o.mini.innerHTML = f.dv().mini
+      }
+      if(o.hour) {
+        o.hour.innerHTML = f.dv().hour
+      }
+      if(o.day) {
+        o.day.innerHTML = f.dv().day
+      }
+      if(o.month) {
+        o.month.innerHTML = f.dv().month
+      }
+      if(o.year) {
+        o.year.innerHTML = f.dv().year
+      }
+      setTimeout(f.ui,1000)
+    }
+  }
+  f.ui()
+}
+
+var sec = document.getElementById('sec')
+var mini = document.getElementById('mini')
+var hour = document.getElementById('hour')
+
+var d = Date.UTC(2017,6,28,23,00)
+
+var obj = {
+  sec: sec,
+  mini: mini,
+  hour: hour
+}
+fnTimeCountDown(d,obj)
